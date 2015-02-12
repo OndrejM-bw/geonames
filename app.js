3b2f6db0b5c75d48c21ad81c5f016e32735a07d9
ondrejm <ondrej.mihalyi@gmail.com>
2015-02-12 01:34:31 +0000

    Search by location name implemented and tested.

--- app.js
+++ app.js
@@ -6,4 +6,38 @@
     var map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
   }
-  google.maps.event.addDomListener(window, 'load', initialize);
+  
+  $(document).ready(function() {
+    var geonames = {
+      username: "mihalyi.bw",
+      getLocations: function(name, callback) {
+        $.getJSON("http://api.geonames.org/searchJSON?username=" + this.username + "&name_equals=" + name + "&style=FULL", callback);
+      }
+    }
+    
+    var app = {
+      getSearchButton: function() {
+        return $("#searchButton");
+      },
+      getLocation: function() {
+        return $("#location").val();
+      },
+      start: function() {
+        var self = this;
+        this.getSearchButton().click(function(e) {
+          e.preventDefault();
+          self.search(self.getLocation());
+        })
+      },
+      search: function(location) {
+        var self = this;
+        geonames.getLocations(location, function(data) {
+          console.log(data);
+          self.searchResults = data.geonames;
+        });
+      }
+    };
+    
+    app.start();
+    window.app = app;
+  });
--- tests.js
+++ tests.js
@@ -1,10 +1,21 @@
-QUnit.test( "B1 search page is displayed", function( assert ) {
+QUnit.test( "B1: search page is displayed", function( assert ) {
     openInitialPage(assert, function (done) {
         assert.ok( appFrame().hasSearchButton(), "Has search button" );
         assert.ok( appFrame().hasLocationInput(), "Has location field");
         done();
     });
     
+});
+
+QUnit.test( "B3: search by location retrieves coordinates", function( assert ) {
+    openInitialPage(assert, function (done) {
+        appFrame().setLocationValue("Los Angeles");
+        appFrame().submitSearch();
+        setTimeout(function() {
+            assert.ok(appFrame().getWindow().app.searchResults.length > 0);
+            done();
+        }, 1000);
+    });
 });
 
 function openInitialPage(assert, callback) {
@@ -20,11 +31,26 @@ function openInitialPage(assert, callback) {
 function appFrame() {
     return {
         elem: $("#appframe"),
+        getSearchButton: function() {
+            return this.elem.contents().find("#searchButton");
+        },
         hasSearchButton: function() {
-            return this.elem.contents().find("#searchButton").length > 0;
+            return this.getSearchButton().length > 0;
         },
         hasLocationInput: function() {
-            return this.elem.contents().find("#location").length > 0;
+            return this.getLocationElem().length > 0;
+        },
+        getLocationElem: function() {
+            return this.elem.contents().find("#location");
+        },
+        setLocationValue: function(value) {
+            this.getLocationElem().val(value);
+        },
+        submitSearch: function() {
+            this.getSearchButton().click();
+        },
+        getWindow: function() {
+            return this.elem[0].contentWindow || this.elem[0];
         }
 
     };
