63e06ad845c1ec99e04a5b3f8bc34542a9ea89c3
ondrejm <ondrej.mihalyi@gmail.com>
2015-02-12 22:11:53 +0000

    Implemented earthquake retrieval. Improved test engine to wait for external condition with timeout.

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+  function startApp() {
+    $(document).ready(function() {
+      var geonames = newGeonames();
+      var app = newApp(geonames);
+      
+      app.start();
+      window.app = app;
+    });
+  }
+
   function initialize() {
     var mapOptions = {
       center: { lat: -34.397, lng: 150.644},
@@ -7,15 +17,40 @@
         mapOptions);
   }
   
-  $(document).ready(function() {
-    var geonames = {
+  function newGeonames() {
+    return {
       username: "mihalyi.bw",
       getLocations: function(name, callback) {
         $.getJSON("http://api.geonames.org/searchJSON?username=" + this.username + "&name_equals=" + name + "&style=FULL", callback);
+      },
+      getEarthquakes: function(locations, callback) {
+        var i = 0; 
+        var result = [];
+        var self = this;
+        var retrieveData = function(data) {
+          if (data !== undefined) {
+            result = result.concat(data.earthquakes);
+          }
+          if (i < locations.length) {
+            // asynchronous loop for i = 0 to locations.length
+            var loc = locations[i].bbox;
+            $.getJSON("http://api.geonames.org/earthquakesJSON?username=" + self.username 
+              + "&east=" + loc.east  + "&south=" + loc.south  + "&north=" + loc.north  + "&west=" + loc.west, retrieveData);
+            i++;
+          } else {
+            // continue after loop
+            callback(result);
+          }
+        };
+        
+        retrieveData();
       }
-    }
-    
-    var app = {
+    };
+  }
+  
+  function newApp(geonames) {
+    return {
+      geonames: geonames,
       getSearchButton: function() {
         return $("#searchButton");
       },
@@ -31,13 +66,9 @@
       },
       search: function(location) {
         var self = this;
-        geonames.getLocations(location, function(data) {
-          console.log(data);
+        this.geonames.getLocations(location, function(data) {
           self.searchResults = data.geonames;
         });
       }
     };
-    
-    app.start();
-    window.app = app;
-  });
+  }
--- tests.js
+++ tests.js
@@ -11,12 +11,55 @@ QUnit.test( "B3: search by location retrieves coordinates", function( assert ) {
     openInitialPage(assert, function (done) {
         appFrame().setLocationValue("Los Angeles");
         appFrame().submitSearch();
-        setTimeout(function() {
-            assert.ok(appFrame().getWindow().app.searchResults.length > 0);
+        var searchFinished = function() {
+           return appFrame().getWindow().app.searchResults && appFrame().getWindow().app.searchResults.length > 0;
+        }
+        waitForCondition(searchFinished, function() {
+            assert.ok(searchFinished());
             done();
-        }, 1000);
+        });
     });
 });
+
+QUnit.test("B4: from list of locations find earthquakes", function(assert) {
+    var locations = getSampleLocationQueryResult();
+    var geonames = newGeonames();
+    var done = assert.async();
+    geonames.getEarthquakes(locations, function(earthquakes) {
+        assert.notEqual(undefined, earthquakes, "Earthquakes should be filled");
+        assert.notEqual(0, earthquakes.length, "Earthquakes should contain some elements");
+        assert.notEqual(undefined, earthquakes[0].datetime, "Earthquake record should contain datetime");
+        assert.notEqual(undefined, earthquakes[0].lng, "Earthquake record should contain longitude");
+        assert.notEqual(undefined, earthquakes[0].lat, "Earthquake record should contain latitude");
+        assert.notEqual(undefined, earthquakes[0].magnitude, "Earthquake record should contain magnitude");
+        done();
+    });
+});
+
+
+/* helper functions
+===============================
+*/
+
+function waitForCondition(conditionEvaluator, callback) {
+    var retryCount = 100;
+    var waitIfNotMet = function() {
+        setTimeout(function() {
+            if (conditionEvaluator()) {
+                callback(true);
+            } else {
+                if (retryCount > 0) {
+                    retryCount--;
+                    waitIfNotMet();
+                } else {
+                    callback(false);
+                }
+            }
+        }, 100);
+    };
+    
+    waitIfNotMet();
+}
 
 function openInitialPage(assert, callback) {
     var done = assert.async();
@@ -55,3 +98,41 @@ function appFrame() {
 
     };
 }
+
+function getSampleLocationQueryResult() {
+    return  [{"timezone":{"gmtOffset":-8,"timeZoneId":"America/Los_Angeles","dstOffset":-7},
+              "bbox":{"east":-118.155289,"south":33.703652,"north":34.337306,"west":-118.668176},
+              "asciiName":"Los Angeles","countryId":"6252001","fcl":"P","score":142.10707092285156,
+              "adminId2":"5368381","countryCode":"US","adminId1":"5332921","lat":"34.05223",
+              "fcode":"PPLA2","continentCode":"NA","elevation":89,"adminCode2":"037","adminCode1":"CA",
+              "lng":"-118.24368","geonameId":5368361,"toponymName":"Los Angeles","population":3792621,
+              "adminName5":"","adminName4":"","adminName3":"",
+              "alternateNames":[{"name":"Лос-Анџелес","lang":"ab"},{"name":"Los Angeles","lang":"af"}],
+              "adminName2":"Los Angeles County","name":"Los Angeles","fclName":"city, village,...",
+              "countryName":"United States","fcodeName":"seat of a second-order administrative division","adminName1":"California"},
+             {"timezone":{"gmtOffset":-3,"timeZoneId":"America/Santiago","dstOffset":-4},
+              "bbox":{"east":-72.29591207801225,"south":-37.51559079048527,"north":-37.42387320951473,"west":-72.41140192198775},
+              "asciiName":"Los Angeles","countryId":"3895114","fcl":"P","score":62.03242874145508,"adminId2":"3898381","adminId3":"8261144",
+              "countryCode":"CL","adminId1":"3898380","lat":"-37.46973","fcode":"PPL","continentCode":"SA",
+              "adminCode2":"83","adminCode3":"08301","adminCode1":"06","lng":"-72.35366","geonameId":3882428,
+              "toponymName":"Los Ángeles","population":125430,"adminName5":"","adminName4":"","adminName3":"Los Angeles",
+              "alternateNames":[{"name":"Los Anceles","lang":"az"},{"name":"לוס אנחלס","lang":"he"}],
+              "adminName2":"Provincia de Biobío","name":"Los Ángeles","fclName":"city, village,...","countryName":"Chile",
+              "fcodeName":"populated place","adminName1":"Biobío"},
+             {"timezone":{"gmtOffset":-3,"timeZoneId":"America/Argentina/Catamarca","dstOffset":-3},
+              "bbox":{"east":-65.97310197500876,"south":-28.492326948751757,"north":-28.474339651248247,"west":-65.99356462499124},
+              "asciiName":"Los Angeles","countryId":"3865483","fcl":"P","score":55.483489990234375,"countryCode":"AR","adminId1":"3862286",
+              "lat":"-28.48333","fcode":"PPL","continentCode":"SA","adminCode1":"02","lng":"-65.98333","geonameId":3846493,"toponymName":"Los Ángeles",
+              "population":0,"adminName5":"","adminName4":"","adminName3":"",
+              "alternateNames":[{"name":"http://en.wikipedia.org/wiki/Los_%C3%81ngeles%2C_Catamarca","lang":"link"}],
+              "adminName2":"","name":"Los Ángeles","fclName":"city, village,...","countryName":"Argentina","fcodeName":"populated place",
+              "adminName1":"Catamarca"},
+             {"timezone":{"gmtOffset":-3,"timeZoneId":"America/Santiago","dstOffset":-4},
+              "bbox":{"east":-72.04075959667718,"south":-37.66292455727078,"north":-37.17850689958166,"west":-72.68447087071509},
+              "asciiName":"Los Angeles","countryId":"3895114","fcl":"A","score":54.92584991455078,"adminId2":"3898381","adminId3":"8261144","countryCode":"CL",
+              "adminId1":"3898380","lat":"-37.40792","fcode":"ADM3","continentCode":"SA","adminCode2":"83","adminCode3":"08301","adminCode1":"06","lng":"-72.32774",
+              "geonameId":8261144,"toponymName":"Los Angeles","population":0,"adminName5":"","adminName4":"","adminName3":"Los Angeles","adminName2":"Provincia de Biobío",
+              "name":"Los Angeles","fclName":"country, state, region,...","countryName":"Chile","fcodeName":"third-order administrative division","adminName1":"Biobío"}
+            ];
+}
+
